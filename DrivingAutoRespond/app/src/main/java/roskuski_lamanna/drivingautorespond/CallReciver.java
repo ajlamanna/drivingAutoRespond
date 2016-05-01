8456e78b187647963f68c4a32f6bf7946b8a9b06
Andrew Roskuski <aroskuski@gmail.com>
2016-05-01 18:32:36 -0400

    Basic CallReciever and made it so the MainActivity requests permissions on 6.0+

---
+++ DrivingAutoRespond/app/src/main/java/roskuski_lamanna/drivingautorespond/CallReciver.java
@@ -0,0 +1,20 @@
+package roskuski_lamanna.drivingautorespond;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+
+public class CallReciver extends BroadcastReceiver {
+    public CallReciver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)){
+            String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            System.out.println(number);
+            //TODO: Send this somewhere for processing
+        }
+    }
+}
--- DrivingAutoRespond/app/src/main/java/roskuski_lamanna/drivingautorespond/MainActivity.java
+++ DrivingAutoRespond/app/src/main/java/roskuski_lamanna/drivingautorespond/MainActivity.java
@@ -1,5 +1,9 @@
 package roskuski_lamanna.drivingautorespond;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.RECEIVE_SMS},
+                    0);
+        }
     }
 
     @Override
@@ -33,5 +43,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        for(Integer i : grantResults){
+            if(i != PackageManager.PERMISSION_GRANTED){
+                this.finish();
+                return;
+            }
+        }
     }
 }
