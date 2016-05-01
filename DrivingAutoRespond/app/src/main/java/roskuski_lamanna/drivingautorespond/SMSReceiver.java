197a7fd4766b4afbc4b00837acf865f63a76a161
Andrew Roskuski <aroskuski@gmail.com>
2016-05-01 16:56:20 -0400

    Basic SMSReciever and some config updates

---
+++ DrivingAutoRespond/app/src/main/java/roskuski_lamanna/drivingautorespond/SMSReceiver.java
@@ -0,0 +1,34 @@
+package roskuski_lamanna.drivingautorespond;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SMSReceiver extends BroadcastReceiver {
+    public SMSReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+
+        System.out.println(messages.length);
+
+        Set<String> senders = new HashSet<String>();
+
+        for(SmsMessage message : messages) {
+            message.getOriginatingAddress();
+        }
+
+
+        for(String s : senders){
+            System.out.println(s);
+        }
+        //TODO: Send list of senders somewhere else
+    }
+}
